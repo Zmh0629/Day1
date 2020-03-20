88bab37ca3a531091109c49acc6d297f1fb508a2
好名字 <@DESKTOP-UA3LACG>
2020-03-20 21:24:10 +0800

    第一次提交

---
+++ Day3_20/Day3_20/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我最帅，万人迷");
+        }
+    }
+}
